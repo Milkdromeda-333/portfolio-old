23d09e6c5fc9f4a41133b3d69c0f3da9af6edecf
Anjanique <anjaniquemackey@gmail.com>
2023-01-08 21:59:13 -0800

    finishes navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useState } from "react";
 import Logo from "../assets/logo2.gif"
+import "../assets/hamburger.css"
 
 export default function Navbar() {
+
+    const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+    const handleHamburger = () => setIsHamburgerActive(prev => !prev);
 
     return (
         <nav className="navbar">
@@ -9,7 +15,15 @@ export default function Navbar() {
                 <img src={Logo} alt="logo" className="logo" />
             </Link>
 
-            <div className="nav-links">
+            <button className={`hamburger hamburger--elastic ${isHamburgerActive ? "is-active" : ""}`} type="button" onClick={handleHamburger} aria-label="Open the menu" aria-expanded={isHamburgerActive ? "true" : "false"}>
+
+                <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                </span>
+
+            </button>  
+
+            <div className={`nav-links ${isHamburgerActive ? "is-active" : "" }`} >
                 <Link to="/">Home</Link>
                 <Link to="/portfolio">Portfolio</Link>
                 <Link to="/contact">Contact me</Link>
